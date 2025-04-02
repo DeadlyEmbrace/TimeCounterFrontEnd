771309d3aa5193115c912fd038abdaca7fdf229d
Ralph Viktor <ralph.viktor@epiuse.com>
2025-04-02 11:58:51 -0600

    Initial commit
    - Initial commit

---
+++ TimeCounterFrontend/Models/TimeReadmodel.cs
@@ -0,0 +1,17 @@
+﻿namespace TimeCounterFrontend.Models;
+
+/// <summary>
+/// Model to return the time and number of request the server has handled
+/// </summary>
+public class TimeReadmodel
+{
+    /// <summary>
+    /// Current server time
+    /// </summary>
+    public DateTime ServerTime { get; set; }
+
+    /// <summary>
+    /// The number of request made to the server
+    /// </summary>
+    public int RequestCounter { get; set; }
+}
---
+++ TimeCounterFrontend/Program.cs
@@ -0,0 +1,34 @@
+using Serilog;
+using TimeCounterFrontend.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Configuration["SplunkRum:Token"] = Environment.GetEnvironmentVariable("SPLUNK_RUM_TOKEN") ?? "";
+builder.Configuration["SplunkRum:Environment"] = Environment.GetEnvironmentVariable("SPLUNK_RUM_ENVIRONMENT") ?? "";
+builder.Configuration["Serilog:Seq:ServerUrl"] = Environment.GetEnvironmentVariable("SEQ_SERVER_URL") ?? "";
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .WriteTo.Seq(context.Configuration["Serilog:Seq:ServerUrl"] ?? "http://localhost:5341")
+    .WriteTo.Console());
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
